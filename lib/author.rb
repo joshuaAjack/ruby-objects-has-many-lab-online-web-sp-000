ac6085cdb55a1d18f6acf42d5e7c48268a54b738
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-12 20:05:59 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,26 @@
 class Author 
-  attr_accessor :name, :posts
+ attr_accessor :name, :posts
 
   def initialize(name)
     @name = name
     @posts = []
   end
 
-  def add_post(posts)
-    @post << posts 
-    posts.author = self
+  def add_post(post)
+    post.author = self
+    @posts << post
   end
 
   def add_post_by_title(title)
-    post = Post.new(posts)
-    post.artist = self
-    @posts << posts
+    post = Post.new(title)
+    add_post(post)
   end
 
   def self.post_count
     Post.all.length
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
+  def posts
+    Post.all.select {|post| post.author == self}
   end
-end
+end 
